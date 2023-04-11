b74e3e9d952a5af530d076329caf0b59290696dc
Crowdbotics <team@crowdbotics.com>
2023-04-11 14:33:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "geotek-39835",
+  url: "https://geotek-39835.botics.co",
+  api: "https://geotek-39835.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
